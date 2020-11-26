4fb7bce2943d351bd32c46a6bdc77bacfcecc495
Jorge <stivenflo1993@gmail.com>
2020-11-25 18:54:43 -0600

    BD

--- docs/scripts/01_create_database.sql
+++ docs/scripts/01_create_database.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA `nw202003` ;
-CREATE USER 'demouser'@'%' IDENTIFIED BY 'carm3l1t4s';
-GRANT ALL ON demoalgorith.* TO 'demouser'@'%';
+--CREATE USER 'demouser'@'%' IDENTIFIED BY 'carm3l1t4s';
+--GRANT ALL ON demoalgorith.* TO 'demouser'@'%';
 
 --Mysql
---CREATE USER 'stivenflo'@'%' IDENTIFIED BY 'qwerty';
---GRANT ALL ON nw202003.* TO 'stivenflo'@'%';
+CREATE USER 'stivenflo'@'%' IDENTIFIED BY 'qwerty';
+GRANT ALL ON nw202003.* TO 'stivenflo'@'%';
